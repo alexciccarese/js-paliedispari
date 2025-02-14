5af92c06d26000ef9fc94f49b27aea93a53226e6
alexciccarese <alex.ciccarese@gmail.com>
2025-02-14 16:55:37 +0100

    add pc random number

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -1,9 +1,22 @@
 /* Pari e Dispari
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto. */
 
-
+// scelta utente 
 const userChoise = prompt('Scegli pari o dispari')
 
 const userNumber = Number(prompt('Scegli un numero da 1 a 5'))
+
+
+// il pc genera i suoi numeri (funzione)
+function pcRandom() {
+  const pcNumber = Math.ceil(Math.random() * 5)
+  return pcNumber
+}
+const pcNumber = pcRandom()
+console.log(pcNumber);
+
+
+// somma di numero utente più pc
+
 
 
